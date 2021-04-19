8e98d147e3c48fa098d798da49af85c153002e04
tapi1300 <pedop4@gmail.com>
2021-04-19 20:17:16 +0200

    Creemos que la capa de mision está done

--- cognitive_arch/fsm/MyBT.hpp
+++ cognitive_arch/fsm/MyBT.hpp
@@ -1,9 +1,22 @@
-// NOLINT (legal/copyright) 
+// Copyright 2021 El Grupo Del Flow
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #ifndef MYBT_H_
 #define MYBT_H_
 
 #include <string>
+#include <memory>
 
 #include "std_msgs/msg/string.hpp"
 #include "rclcpp_cascade_lifecycle/rclcpp_cascade_lifecycle.hpp"
@@ -39,9 +52,10 @@ public:
   virtual bool State1_2_State2() {return false;}
   virtual bool Initial_2_State1() {return false;}
   virtual bool State2_2_Initial() {return false;}
-
-
+  
+  void init();
   void tick();
+  bool get_result(){return executor_client_->getResult().value().success;}
 
 protected:
   rclcpp::Time state_ts_;
