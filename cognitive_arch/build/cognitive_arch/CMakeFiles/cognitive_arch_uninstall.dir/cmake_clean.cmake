56c970909f03bcfa23a52b7bfa318661661e1086
tapi1300 <pedop4@gmail.com>
2021-04-21 19:15:19 +0200

    Arreglado error de commit previo :C

--- cognitive_arch/build/cognitive_arch/CMakeFiles/cognitive_arch_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/cognitive_arch_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/cognitive_arch_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- cognitive_arch/install/cognitive_arch/share/cognitive_arch/cmake/ament_cmake_export_dependencies-extras.cmake
+++
@@ -1,1 +0,0 @@
-/home/achrafnvl/ros2_foxy/src/PySC_ros2/cognitive_arch/build/cognitive_arch/ament_cmake_export_dependencies/ament_cmake_export_dependencies-extras.cmake
