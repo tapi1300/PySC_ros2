923117a686807eff07a0fd68bcc335c915048b98
Diego Encabo del Castillo <d.encabo.2018@alumnos.urjc.es>
2021-05-02 14:09:05 +0200

    Probando el filtro

--- cognitive_arch/src/behavior_tree_nodes/Search.cpp
+++ cognitive_arch/src/behavior_tree_nodes/Search.cpp
@@ -15,9 +15,16 @@
 #include <string>
 #include <iostream>
 
+
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <image_transport/image_transport.hpp>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.hpp>
 #include "cognitive_arch/behavior_tree_nodes/Search.hpp"
 #include "geometry_msgs/msg/twist.hpp"
 #include "behaviortree_cpp_v3/behavior_tree.h"
+
 
 using std::placeholders::_1;
 using std::placeholders::_2;
@@ -28,8 +35,30 @@ namespace plansys2_search
 
 void callback(const sensor_msgs::msg::Image::SharedPtr msg)
 {
-  std::cout << "TUS MUERTOS" << std::endl;
-}
+  cv_bridge::CvImagePtr cv_ptr;
+
+  try{
+    cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+  }
+  catch(cv_bridge::Exception &e){
+    // ROS_ERROR("cv_bridge exception %s", e.what());
+    return;
+  }
+
+  cv::Mat hsv_image;
+  cv::Mat low_mask, high_mask;
+
+  cv::cvtColor(cv_ptr->image, hsv_image, cv::COLOR_BGR2HSV);
+
+  cv::inRange(hsv_image, cv::Scalar(0, 70, 50), cv::Scalar(10, 255, 255), low_mask);
+  cv::inRange(hsv_image, cv::Scalar(170, 70, 50), cv::Scalar(180, 255, 255), high_mask);
+
+  cv::Mat mask = low_mask + high_mask;
+
+  cv::bitwise_and(hsv_image, hsv_image, mask);
+
+  cv::imshow("Prueba", hsv_image);
+  }
 
 Search::Search(
   const std::string & xml_tag_name,
@@ -43,8 +72,6 @@ Search::Search(
       "/kinect_color/image_raw", rclcpp::SensorDataQoS(), callback);
     
 }
-
-
 
 void
 Search::halt()
