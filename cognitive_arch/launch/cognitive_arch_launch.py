b2b21dac7092088c1bcac65f4b1ca7a8e9f5fd7a
Diego Encabo del Castillo <d.encabo.2018@alumnos.urjc.es>
2021-05-17 17:09:38 +0200

    Interesantes los puntos

--- cognitive_arch/launch/cognitive_arch_launch.py
+++ cognitive_arch/launch/cognitive_arch_launch.py
@@ -21,7 +21,7 @@ from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, SetE
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
-import gb_attention
+# import gb_attention
 
 
 def generate_launch_description():
